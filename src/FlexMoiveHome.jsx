b5f5b1989390c03bed2b5abf907cc349ad479a01
Cyril Esin <cyrilesin214@email.com>
2026-02-27 22:46:58 +0100

    set the searchBar an position well

--- src/FlexMoiveHome.jsx
+++ src/FlexMoiveHome.jsx
@@ -1,22 +1,63 @@
 import "./App.css";
-import { populer, trendings } from "../src/api/tmbD.js";
+import { populer, searchButton, topRated, trendings, upComings } from "../src/api/tmbD.js";
 import { useState, useEffect } from "react";
 import { RotateLoader } from "react-spinners";
 export function Homepage() {
     const [weeklyTrends, setweeklyTrends] = useState([]);
     const [populerMovie, setPopulerMovie] = useState([]);
-
-
+    const [ratedMovie, setRatedMovie] = useState([]);
+    const [upComingMovies, setUpComingMovies] = useState([]);
+    const [gridHandle, setGridHandle] = useState(false);
+    const [populerG, setPopulerG] = useState("movie-flex");
+    const [cards, setCards] = useState("movie-card");
+    const [infos, setInfos] = useState("movie-poster");
+    const [movieTitle, setMovieTitle] = useState("movie-title");
+    const [loading, setLoading] = useState(false);
+    const [movieSearch, setMovieSearch] = useState("");
+    const [searchedMovie, setSearchedMovie] = useState([])
     useEffect(() => {
         trendings().then(data => setweeklyTrends(data.results));
-        console.log(weeklyTrends)
+        populer().then(data => setPopulerMovie(data.results));
+        topRated().then(data => setRatedMovie(data.results));
+        upComings().then(data => setUpComingMovies(data.results));
     }, [])
+
+
+    const handleSearch = () => {
+        searchButton(movieSearch).then(data => setSearchedMovie(data.results));
+    }
 
 
     useEffect(() => {
-        populer().then(data => setPopulerMovie(data.results));
+        if (!weeklyTrends) {
+            setLoading(false)
+        } else {
+            setLoading(true);
+        }
+    }, [weeklyTrends])
 
-    }, [])
+
+
+
+    const showGrid = () => {
+        const condition = gridHandle ? "movie-grid" : "movie-flex";
+        const cardCondition = gridHandle ? "movie-cardrGrid" : "movie-card";
+        const infoCondition = gridHandle ? "movie-posterGrid" : "movie-poster";
+        const tilte = gridHandle ? "movie-titleGrid" : "movie-title";
+        setPopulerG(condition);
+        setCards(cardCondition);
+        setInfos(infoCondition);
+        setMovieTitle(tilte);
+    }
+
+
+    const embedFunction = (movieId) async => {
+        try {
+            const resp = await fetch()
+        }
+
+    }
+
 
 
     return (
@@ -29,12 +70,24 @@ export function Homepage() {
                 <header className="navbar">
                     <div className="logo">
                         <i className="fas fa-film"></i>
+
                         <span>MOVIEFLEX</span>
                     </div>
                     <div className="search-wrapper">
-                        <i className="fas fa-search search-icon"></i>
-                        <input type="text" className="search-bar" placeholder="Search movies..." />
+                        {/* <i className="fas fa-search search-icon"></i> */}
+                        <input type="text"
+                            className="search-bar"
+                            placeholder="Search movies..."
+                            onChange={(e) => {
+                                setMovieSearch(e.target.value);
+                            }}
+                        />
+
                     </div>
+                    <button className="buttonsWrap" onClick={() => {
+                        handleSearch();
+                        console.log(searchedMovie);
+                    }}><i className="fas fa-search search-icon"></i></button>
                     <div className="profile">
                         <i className="fas fa-user-circle"></i>
                     </div>
@@ -43,64 +96,113 @@ export function Homepage() {
 
                 <main>
 
+                    {searchedMovie && <>
+                        {movieSearch && <div className="resultsPosition"><div className="resultbar"><div className="exitIcon"><button className="exit" onClick={() => {
+                            setSearchedMovie(prev => !prev);
+                        }}>&times;</button>
+                        </div> <h2 className=" forResult">Result For {movieSearch}</h2> </div></div>}
+
+                        <div className="movie-grid whileResult">
+
+                            {
+                                searchedMovie.map((movie) => {
+                                    return (
+                                        <div className="movie-cardGrid" key={movie.id}>
+                                            <div className="movie-posterGrid" ><img src={!movie.poster_path ? "/gallery-svgrepo-com.svg" : `https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} className="moviePic" /></div>
+                                            <div className="movie-info">
+                                                <h3 className="movie-title">{movie.title}</h3>
+                                                <span className="movie-year">{movie.release_date}</span>
+                                                <button className="favorite-btn"><i className="far fa-heart"></i></button>
+                                            </div>
+                                        </div>
+
+                                    )
+
+                                })
+                            }
+                        </div> </>}
+
                     <h2 className="section-title">Recommended for you</h2>
 
 
                     <div className="seeAll">
                         <h1 className="category">🔥Weekly Trends</h1>
-                        <h3 className="seeButton">See All</h3>
+                        <h3 className="seeButton"
+                            onClick={() => {
+                                showGrid();
+                                setGridHandle((prev) => !prev)
+                            }}
+                        >See All</h3>
                     </div>
 
 
-                    <div className="movie-flex">
+                    {loading ?
+                        <div className={populerG}>
 
-                        {weeklyTrends.map((movie) => {
+                            {
+                                weeklyTrends.map((movie) => {
+                                    return (
+                                        <div className={cards} key={movie.id}>
+                                            <div className={infos} ><img src={!movie.poster_path ? "/gallery-svgrepo-com.svg" : `https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} className="moviePic" /></div>
+                                            <div className="movie-info">
+                                                <h3 className={movieTitle}>{movie.title}</h3>
+                                                <span className="movie-year">{movie.release_date}</span>
+                                                <button className="favorite-btn"><i className="far fa-heart"></i></button>
+                                            </div>
+                                        </div>
+
+                                    )
+
+                                })
+                            }
+                        </div> : <RotateLoader size={50} color="white" />}
+
+                    <div className="seeAll">
+                        <h1 className="category">🚀Populer</h1>
+                        <h3 className="seeButton"
+                            onClick={() => {
+                                showGrid();
+                                setGridHandle((prev) => !prev)
+                            }}
+
+                        >See All</h3>
+                    </div>
+                    <div className={populerG}>
+
+                        {populerMovie.map((movie) => {
                             return (
-                                <div className="movie-card" key={movie.id}>
-                                    <div className="movie-poster" ><img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} className="moviePic" /></div>
+
+                                <div className={cards} key={movie.id}>
+                                    <div className={infos} ><img src={!movie.poster_path ? "/gallery-svgrepo-com.svg" : `https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} className="moviePic" /></div>
                                     <div className="movie-info">
-                                        <h3 className="movie-title">{movie.title}</h3>
+                                        <h3 className={movieTitle}>{movie.title}</h3>
                                         <span className="movie-year">{movie.release_date}</span>
                                         <button className="favorite-btn"><i className="far fa-heart"></i></button>
                                     </div>
                                 </div>
-
                             )
-
                         })}
                     </div>
 
                     <div className="seeAll">
-                        <h1 className="category">🚀Populer</h1>
-                        <h3 className="seeButton">See All</h3>
-                    </div>
-                    <div className="movie-flex">
-
-                        {populerMovie.map((movie) => {
-                            return (
-
-                                <div className="movie-card" key={movie.id}>
-                                    <div className="movie-poster" ><img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} className="moviePic" /></div>
-                                    <div className="movie-info">
-                                        <h3 className="movie-title">{movie.title}</h3>
-                                        <span className="movie-year">{movie.release_date}</span>
-                                        <button className="favorite-btn"><i className="far fa-heart"></i></button>
-                                    </div>
-                                </div>
-                            )
-                        })}
+                        <h1 className="category">⭐Top Rated</h1>
+                        <h3 className="seeButton" onClick={() => {
+                            showGrid();
+                            setGridHandle((prev) => !prev)
+                        }}>See All</h3>
                     </div>
 
-                    <div className="movie-flex">
+                    <div className={populerG}>
 
-                        {populerMovie.map((movie) => {
+                        {ratedMovie.map((movie) => {
                             return (
 
-                                <div className="movie-card" key={movie.id}>
-                                    <div className="movie-poster" ><img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} className="moviePic" /></div>
+                                <div className={cards} key={movie.id}>
+                                    <div className={infos} ><img src={!movie.poster_path ? "/gallery-svgrepo-com.svg" : `https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} className="moviePic" /></div>
                                     <div className="movie-info">
-                                        <h3 className="movie-title">{movie.title}</h3>
+                                        <h3 className={movieTitle}>{movie.title}</h3>
                                         <span className="movie-year">{movie.release_date}</span>
+                                        <span className="movie-year">{Math.round(movie.vote_average)}⭐ <small>{`(${movie.vote_average}/10)`}</small> <small><i>{`${movie.vote_count} vote`}</i></small></span>
                                         <button className="favorite-btn"><i className="far fa-heart"></i></button>
                                     </div>
                                 </div>
@@ -108,6 +210,31 @@ export function Homepage() {
                         })}
 
 
+
+                    </div>
+
+                    <div className="seeAll">
+                        <h1 className="category">⬆️ Up Next</h1>
+                        <h3 className="seeButton" onClick={() => {
+                            showGrid();
+                            setGridHandle((prev) => !prev)
+                        }}>See All</h3>
+                    </div>
+
+                    <div className="movie-flex">
+                        {upComingMovies.map((movie) => {
+                            return (
+                                <div className={cards} key={movie.id}>
+                                    <div className={infos} ><img src={!movie.poster_path ? "/gallery-svgrepo-com.svg" : `https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} className="moviePic" /></div>
+                                    <div className="movie-info">
+                                        <h3 className={movieTitle}>{movie.title}</h3>
+                                        <span className="movie-year">{movie.release_date}</span>
+                                        {/* <span className="movie-year">{Math.round(movie.vote_average)}⭐ <small>{`(${movie.vote_average}/10)`}</small> <small><i>{`${movie.vote_count} vote`}</i></small></span> */}
+                                        <button className="favorite-btn"><i className="far fa-heart"></i></button>
+                                    </div>
+                                </div>
+                            )
+                        })}
 
                     </div>
 
--- src/api/tmbD.js
+++ src/api/tmbD.js
@@ -11,4 +11,21 @@ export const populer = async () => {
     const resp = await fetch(`${baseUrl}/movie/popular?api_key=${apiKey}`);
     return resp.json();
 }
+export const topRated = async () => {
+    const resp = await fetch(`${baseUrl}/movie/top_rated?api_key=${apiKey}`);
+    return resp.json();
+}
+
+const today = new Date().toISOString().split("T")[0];
+
+export const upComings = async () => {
+    const resp = await fetch(`${baseUrl}/discover/movie?api_key=${apiKey}&region=NG&sort_by=primary_release_date.asc&primary_release_date.gte=${today}`);
+    return resp.json();
+}
+
+export const searchButton = async (query) => {
+    const resp = await fetch(`${baseUrl}/search/movie?api_key=${apiKey}&query=${query}`);
+
+    return resp.json();
+}
 
